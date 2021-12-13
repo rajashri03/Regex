6d7ed38d9d0fcecfa3d3611941ba809019ceea9d
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-13 14:45:51 +0530

    UserRegistration:Please Enter Valid Name

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UserReg;
+using System.Text.RegularExpressions;
+UserRegistration User=new UserRegistration();
+bool flag = true;
+while(flag)
+{
+    Console.WriteLine("Select:\n1)Enter Valid First Name\n");
+    int op = Convert.ToInt16(Console.ReadLine());
+    switch(op)
+    {
+        case 1:
+            User.FirstName();
+            break;
+    }
+}
---
+++ Regex/Regex/UserRegistration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace UserReg
+{
+    public class UserRegistration
+    {
+        public string FirstName()
+        {
+            Console.WriteLine("Enter First Name");
+            string Firstname = Console.ReadLine();
+            string Fname = @"[A-Z]{1}[a-z]{2}$";
+            Regex rg = new Regex(Fname);
+
+            if (rg.IsMatch(Firstname))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Name is Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Plase Enter First Letter Capital and Length of the name upto 3 only");
+                Console.ResetColor();
+            }
+
+            Console.WriteLine();
+            return default;
+        }
+    }
+}
